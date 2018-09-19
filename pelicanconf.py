dc57a329ad55bccb0b717a3cf98c0d45a3d460ed
Michiel Scholten <michiel@diginaut.net>
2018-09-19 13:59:10 +0200

    Better indentation, added Mastodon profile link

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,17 +38,20 @@ LINKS = (('Felienne', 'http://www.felienne.com/'),
         )
 
 # Social widget
-SOCIAL = (('Google+', 'http://plus.google.com/u/0/+MichielScholten',
-           'fa fa-google-plus-square fa-fw fa-lg'),
-          ('Twitter', 'https://twitter.com/michielscholten',
-           'fa fa-twitter-square fa-fw fa-lg'),
-          #('LinkedIn', 'http://linkedin-url',
-          # 'fa fa-linkedin-square fa-fw fa-lg'),
-          #('BitBucket', 'http://bitbucket.org/username',
-          # 'fa fa-bitbucket-square fa-fw fa-lg'),
-          ('GitHub', 'http://github.com/aquatix',
-           'fa fa-github-square fa-fw fa-lg'),
-         )
+SOCIAL = (
+    ('Mastodon', 'https://mastodon.social/@diginaut',
+     'fa fa-mastodon-square fa-fw fa-lg'),
+    ('Twitter', 'https://twitter.com/michielscholten',
+     'fa fa-twitter-square fa-fw fa-lg'),
+    #('LinkedIn', 'http://linkedin-url',
+    # 'fa fa-linkedin-square fa-fw fa-lg'),
+    #('BitBucket', 'http://bitbucket.org/username',
+    # 'fa fa-bitbucket-square fa-fw fa-lg'),
+    ('GitHub', 'https://github.com/aquatix',
+     'fa fa-github-square fa-fw fa-lg'),
+    ('Google+', 'http://plus.google.com/u/0/+MichielScholten',
+     'fa fa-google-plus-square fa-fw fa-lg'),
+    )
 
 # Social metadata
 OPEN_GRAPH = True
