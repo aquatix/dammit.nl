d3bbb1bc9ad87eea8d7ff5afe663f333b2b0df1f
Michiel Scholten <michiel@diginaut.net>
2025-01-07 18:54:14 +0100

    A bit more whitespace under subheadings, and the latest highlighting

--- pelicanconf.py
+++ pelicanconf.py
@@ -84,12 +84,12 @@ BOOTSTRAP_CSS = 'https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min
 
 # Enable highlight.js
 THEME_CSS_OVERRIDES = [
-    '/css/dammit_bootstrap.css?20241116a',
+    '/css/dammit_bootstrap.css?20250107a',
     #'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/styles/base16/3024.min.css',
-    'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.10.0/build/styles/devibeans.min.css',
+    'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/styles/devibeans.min.css',
 ]
 THEME_JS_OVERRIDES = [
-    'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.10.0/build/highlight.min.js',
+    'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/highlight.min.js',
     '/js/dammit.js',
 ]
 
