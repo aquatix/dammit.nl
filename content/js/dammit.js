ebd44bb4c99aeeb097fc8b301f83b47fcbdc3ede
Michiel Scholten <michiel@diginaut.net>
2025-10-11 12:56:11 +0200

    First try on copy-to-clipboard button on code blocks

--- content/js/dammit.js
+++ content/js/dammit.js
@@ -1,1 +1,21 @@
+/* Initialise highlight.js */
 hljs.highlightAll();
+
+/* Add copy to clipboard to all code blocks */
+let blocks = document.querySelectorAll("pre:has(code)");
+
+blocks.forEach((block) => {
+  let button = block.querySelector("button");
+
+  // handle click event
+  button.addEventListener("click", async () => {
+    await copyCode(block);
+  });
+});
+
+async function copyCode(block) {
+  let code = block.querySelector("code");
+  let text = code.innerText;
+
+  await navigator.clipboard.writeText(text);
+}
