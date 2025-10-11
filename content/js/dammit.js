ca9279187f547059a304a43c83034a7bca084cd0
Michiel Scholten <michiel@diginaut.net>
2025-10-11 13:55:29 +0200

    Working copy button!

--- content/js/dammit.js
+++ content/js/dammit.js
@@ -3,19 +3,30 @@ hljs.highlightAll();
 
 /* Add copy to clipboard to all code blocks */
 let blocks = document.querySelectorAll("pre:has(code)");
+let copyButtonLabel = "Copy Code";
 
 blocks.forEach((block) => {
-  let button = block.querySelector("button");
+  // Add a copy button to all blocks.
+  let button = document.createElement("button");
+  button.innerText = copyButtonLabel;
+  block.prepend(button);
 
   // handle click event
   button.addEventListener("click", async () => {
-    await copyCode(block);
+    await copyCode(block, button);
   });
 });
 
-async function copyCode(block) {
+async function copyCode(block, button) {
   let code = block.querySelector("code");
   let text = code.innerText;
 
   await navigator.clipboard.writeText(text);
+
+  // visual feedback that task is completed
+  button.innerText = "Code Copied";
+
+  setTimeout(() => {
+    button.innerText = copyButtonLabel;
+  }, 700);
 }
--- pelicanconf.py
+++ pelicanconf.py
@@ -86,7 +86,7 @@ BOOTSTRAP_CSS = 'https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min
 
 # Enable highlight.js
 THEME_CSS_OVERRIDES = [
-    '/css/dammit_bootstrap.css?20251003c',
+    '/css/dammit_bootstrap.css?20251011a',
     #'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/styles/base16/3024.min.css',
     'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/styles/devibeans.min.css',
     #'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/styles/srcery.min.css',
@@ -94,7 +94,7 @@ THEME_CSS_OVERRIDES = [
 ]
 THEME_JS_OVERRIDES = [
     'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/highlight.min.js',
-    '/js/dammit.js',
+    '/js/dammit.js?20251011a',
 ]
 
 # Several typographical improvements will be incorporated into the generated HTML via the Typogrify library https://pypi.org/project/typogrify/
